17728f8bfd8bdb83b6f0bc88fc3c8545e0cb0de9
kHRYSTAL <723526676@qq.com>
2017-12-10 23:23:43 +0800

    update threesome handler

--- threesomeAndroid/threesome/src/main/java/me/khrystal/threesome/executor/ThreesomeHandler.java
+++ threesomeAndroid/threesome/src/main/java/me/khrystal/threesome/executor/ThreesomeHandler.java
@@ -1,10 +1,17 @@
 package me.khrystal.threesome.executor;
 
+import android.net.Uri;
+import android.util.Log;
+
 import com.google.gson.Gson;
 
+import me.khrystal.threesome.Constants;
 import me.khrystal.threesome.IBridgeAdapter;
 import me.khrystal.threesome.core.BridgeHandler;
 import me.khrystal.threesome.core.CallbackFunction;
+import me.khrystal.threesome.dto.ResponseCode;
+import me.khrystal.threesome.dto.ThreesomeRequest;
+import me.khrystal.threesome.dto.ThreesomeResponse;
 import me.khrystal.threesome.util.gson.GsonHelper;
 
 /**
@@ -28,6 +35,36 @@ public class ThreesomeHandler implements BridgeHandler {
 
     @Override
     public void handler(String data, CallbackFunction function, IBridgeAdapter bridgeAdapter) {
-        // TODO: 17/12/9  
+        data = Uri.decode(data);
+        Log.d(Constants.TAG, "start handle: " + data);
+        if (function == null) {
+            // logic may not be run this
+            Log.e(Constants.TAG, "call back is null");
+            return;
+        }
+
+        ThreesomeRequest request;
+        ThreesomeResponse response;
+
+        try {
+            request = gson.fromJson(data, ThreesomeRequest.class);
+        } catch (Exception ex) {
+            // parse exception
+            response = new ThreesomeResponse();
+            response.code = ResponseCode.ERROR_PARAM;
+            response.errorMsg = ex.getLocalizedMessage();
+            function.onCallback(response);
+            return;
+        }
+        // argument is null
+        if (request == null) {
+            response = new ThreesomeResponse();
+            response.code = ResponseCode.ERROR_PARAM;
+            response.errorMsg = "arguments is null";
+            function.onCallback(response);
+            return;
+        }
+        // handler request
+        taskExecutor.handleRequest(request, function, bridgeAdapter);
     }
 }
